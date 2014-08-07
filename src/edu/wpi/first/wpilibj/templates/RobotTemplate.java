d5a0893ab35b415356bd783b233d62783e418791
PSCA CEO <psca119.tech@gmail.com>
2014-08-06 18:31:27 -0700

    Update RobotTemplate.java

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -2,8 +2,16 @@
 /* Copyright (c) FIRST 2008. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project. Final code revision for FRC TEAM 5014. @author PSCA119        */
 /*----------------------------------------------------------------------------*/
+
+/*
+@ new programmers:
+Please read my comments in the 2014 (this) FRC [Felix v1.1] robot
+code for information on how FRC should be managed! I hope this
+is helpful.
+*/
+
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.DriverStationLCD;
@@ -15,7 +23,7 @@ import edu.wpi.first.wpilibj.Timer;
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
- * directory.
+ * directory. (WPILIB)
  */
 public class RobotTemplate extends IterativeRobot {
 
@@ -28,13 +36,12 @@ public class RobotTemplate extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-        System.out.println("[INFO] ****************************");
-        System.out.println("[INFO] ****************************");
-        System.out.println("[INFO] ****************************");
-        System.out.println("[INFO] ****************************");
         System.out.println("[INFO] ****** ROBOT IS READY FOR USE ******");
-
+        // Tell programmer(s)/Console reader that the Robot is initialized
+        // and will now call console.init(); and then run the loop when the
+        // VM initialization is complete.
         console.init();
+        // clears LCD Driver Station.
         LCD = DriverStationLCD.getInstance();
         LCD.clear();
 
@@ -53,7 +60,6 @@ public class RobotTemplate extends IterativeRobot {
      */
     public void autonomousPeriodic() {
         console.autoRun();
-
     }
 
     public void teleopInit() {
@@ -65,6 +71,8 @@ public class RobotTemplate extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        // Run console instance for Console to call method run();
+        // "Main loop" (while running); NOTE, FMS WILL AUTOMATICALLY DO THIS.
         console.run();
     }
 
